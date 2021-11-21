8a69e41baa6c8c7c6e81f355be82df090793c80d
Phillip Matheson <matheson.phillip@gmail.com>
2021-11-21 13:23:22 +1100

    init

---
+++ screenlayout/.screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 5120x360 --rotate normal --output HDMI-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-1 --off
